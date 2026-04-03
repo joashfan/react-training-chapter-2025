c70b857c0d596de4556beb7165e6c05a46a6ba24
joashfan <hakuro100823@gmail.com>
2026-04-04 06:38:59 +0800

    路徑修正

--- week2/vite.config.js
+++ week2/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 這樣打包後的路徑才會變成相對路徑
   plugins: [react()],
-  base: '/react-training-chapter-2025/',
 })
