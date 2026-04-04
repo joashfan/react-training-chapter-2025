1202c03f876953aa672bcf0194c7f3316e3b4be8
joashfan <hakuro100823@gmail.com>
2026-04-04 19:47:19 +0800

    week4

--- week4/src/App.jsx
+++ week4/src/App.jsx
@@ -8,357 +8,182 @@ import Pagination from "./component/Pagination";
 import "./assets/style.css";
 
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
-const API_PATH = "";
+const API_PATH = "joashcat";
+
+// 建立初始化的資料
+const INITIAL_TEMPLATE_DATA = {
+  id: '',
+  title: '',
+  category: '',
+  origin_price: '',
+  price: '',
+  unit: '',
+  description: '',
+  content: '',
+  is_enabled: false,
+  imageUrl: '',
+  imagesUrl: [],
+  star: '',
+};
 
 function App() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
+  //登入狀態管理(控制顯示登入或產品頁)
   const [isAuth, setIsAuth] = useState(false);
+
+  // 產品資料列表的API狀態
   const [products, setProducts] = useState([]);
+  // 目前選中的產品
+  // const [tempProduct, setTempProduct] = useState();
+  // 正確引入設定綁定DOM元素, Modal 控制相關狀態
+  const productModalRef = useRef(null);
+  // 產品表單資料模板
+  const [templateProduct, setTemplateProduct] = useState(INITIAL_TEMPLATE_DATA);
+  // 狀態驅動畫面的典型應用,Modal 本身只負責「顯示」,行為由 modalType 決定（create / edit / delete）
+  const [modalType, setModalType] = useState(''); // "create", "edit", "delete"
+  // 分頁功能
   const [pagination, setPagination] = useState({});
 
-  const productModalRef = useRef(null);
-  const [modalType, setModalType] = useState("");
-  const [templateData, setTemplateData] = useState({
-    id: "",
-    imageUrl: "",
-    title: "",
-    category: "",
-    unit: "",
-    origin_price: 0,
-    price: 0,
-    description: "",
-    content: "",
-    is_enabled: false,
-    imagesUrl: [],
-  });
-
-  const openModal = (product, type) => {
-    setTemplateData({
-      id: product.id || "",
-      imageUrl: product.imageUrl || "",
-      title: product.title || "",
-      category: product.category || "",
-      unit: product.unit || "",
-      origin_price: product.origin_price || 0,
-      price: product.price || 0,
-      description: product.description || "",
-      content: product.content || "",
-      is_enabled: product.is_enabled || false,
-      imagesUrl: product.imagesUrl || [],
-    });
-    productModalRef.current.show();
-    setModalType(type);
-  };
-
-  const handleFileChange = async (e) => {
-    const url = `${API_BASE}/api/${API_PATH}/admin/upload`;
-
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    try {
-      const formData = new FormData();
-      formData.append("file-to-upload", file);
-
-      let res = await axios.post(url, formData);
-      const uploadedImageUrl = res.data.imageUrl;
-
-      setTemplateData((prevTemplateData) => ({
-        ...prevTemplateData,
-        imageUrl: uploadedImageUrl,
-      }));
-    } catch (error) {
-      console.error("Upload error:", error);
-    }
-  };
-
-  const closeModal = () => {
-    productModalRef.current.hide();
-  };
-
-  const updateProductData = async (id) => {
-    let product;
-    if (modalType === "edit") {
-      product = `product/${id}`;
-    } else {
-      product = `product`;
-    }
-
-    const url = `${API_BASE}/api/${API_PATH}/admin/${product}`;
-    const productData = {
-      data: {
-        ...templateData,
-        origin_price: Number(templateData.origin_price),
-        price: Number(templateData.price),
-        is_enabled: templateData.is_enabled ? 1 : 0,
-        imageUrl: templateData.imageUrl,
-      },
-    };
-
-    try {
-      let response;
-      if (modalType === "edit") {
-        response = await axios.put(url, productData);
-        console.log("更新成功", response.data);
-      } else {
-        response = await axios.post(url, productData);
-        console.log("新增成功", response.data);
-      }
-
-      productModalRef.current.hide();
-      getProductData();
-    } catch (err) {
-      if (modalType === "edit") {
-        console.error("更新失敗", err.response.data.message);
-      } else {
-        console.error("新增失敗", err.response.data.message);
-      }
-    }
-  };
-
-  const delProductData = async (id) => {
-    try {
-      const response = await axios.delete(
-        `${API_BASE}/api/${API_PATH}/admin/product/${id}`
-      );
-      console.log("刪除成功", response.data);
-      productModalRef.current.hide();
-      getProductData();
-    } catch (err) {
-      console.error("刪除失敗", err.response.data.message);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { id, value, type, checked } = e.target;
-
-    if (id === "username" || id === "password") {
-      setFormData((prevData) => ({
-        ...prevData,
-        [id]: value,
-      }));
-    } else {
-      setTemplateData((prevData) => ({
-        ...prevData,
-        [id]: type === "checkbox" ? checked : value,
-      }));
-    }
-  };
-
-  const handleImageChange = (index, value) => {
-    setTemplateData((prevData) => {
-      const newImages = [...prevData.imagesUrl];
-      newImages[index] = value;
-
-      if (
-        value !== "" &&
-        index === newImages.length - 1 &&
-        newImages.length < 5
-      ) {
-        newImages.push("");
-      }
-
-      if (newImages.length > 1 && newImages[newImages.length - 1] === "") {
-        newImages.pop();
-      }
-
-      return { ...prevData, imagesUrl: newImages };
-    });
-  };
-
-  const handleAddImage = () => {
-    setTemplateData((prevData) => ({
-      ...prevData,
-      imagesUrl: [...prevData.imagesUrl, ""],
-    }));
-  };
-
-  const handleRemoveImage = () => {
-    setTemplateData((prevData) => {
-      const newImages = [...prevData.imagesUrl];
-      newImages.pop();
-      return { ...prevData, imagesUrl: newImages };
-    });
-  };
-
-  const getProductData = async (page = 1) => {
+  // 串接API
+  // 設定取得產品資料列表 API (get)
+  const getProducts = async (page = 1) => {
     try {
       const response = await axios.get(
-        `${API_BASE}/api/${API_PATH}/admin/products?page=${page}`
+        `${API_BASE}/api/${API_PATH}/admin/products?page=${page}`,
       );
       setProducts(response.data.products);
-      setPagination(response.data.pagination);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
-      const { token, expired } = response.data;
-      document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
-      axios.defaults.headers.common.Authorization = `${token}`;
-
-      getProductData()
-      setIsAuth(true);
-    } catch (err) {
-      alert(`登入失敗：${err.response.data.message}`);
+      setPagination(response.data.pagination); //分頁功能
+      console.log('產品列表載入成功：', response.data.products);
+    } catch (error) {
+      console.log('取得產品列表失敗：', error.response?.data?.message);
+      alert(
+        '取得產品列表失敗：' + (error.response?.data?.message || error.message),
+      );
     }
   };
 
   useEffect(() => {
-    const token = document.cookie.replace(
-      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
-      "$1"
-    );
-    axios.defaults.headers.common.Authorization = token;
-    productModalRef.current = new bootstrap.Modal("#productModal", {
-      keyboard: false,
+    const token = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('hexToken='))
+      ?.split('=')[1];
+    // 登入成功後，將Token設定到axios的預設Header，之後所有API請求都會自動帶上Token
+    // 修改實體建立時所指派的預設配置
+    if (token) {
+      // 如果真的有取到token才會放入Header上面
+      axios.defaults.headers.common['Authorization'] = token;
+    }
+    // 取得DOM元素
+    productModalRef.current = new bootstrap.Modal('#productModal', {
+      keybord: false,
     });
 
-    document
-      .querySelector("#productModal")
-      .addEventListener("hide.bs.modal", () => {
-        if (document.activeElement instanceof HTMLElement) {
-          document.activeElement.blur();
-        }
-      });
-
-      const checkAdmin = async () => {
-        try {
-          await axios.post(`${API_BASE}/api/user/check`);
-          getProductData();
-          setIsAuth(true);
-        } catch (err) {
-          console.log(err.response.data.message);
-        }
-      };
-    checkAdmin();
+    // 登入驗證
+    const checkLogin = async () => {
+      try {
+        const response = await axios.post(`${API_BASE}/api/user/check`);
+        console.log(response.data);
+        setIsAuth(true);
+        getProducts();
+      } catch (error) {
+        console.log(error.response?.data.message);
+      }
+    };
+    //呼叫checkLogin
+    checkLogin();
   }, []);
+
+  //寫一個openModal的方式，使用 ref 控制 Modal(互動視窗)
+  const openModal = (type, product) => {
+    // console.log(product);
+    // 設定 Modal 類型並顯示
+    setModalType(type);
+    // 正確：用 setState 並回傳新物件
+    setTemplateProduct({
+      ...INITIAL_TEMPLATE_DATA, //每次都從乾淨的初始狀態開始
+      ...product,
+    });
+    productModalRef.current.show();
+  };
+  // 寫一個closeModal的方式，使用 ref 控制 Modal(互動視窗)
+  const closeModal = () => {
+    productModalRef.current.hide();
+  };
 
   return (
     <>
-      {isAuth ? (
-        <div>
-          <div className="container">
-            <div className="d-flex justify-content-between mt-4">
-              <button
-                className="btn btn-primary"
-                onClick={() => openModal("", "new")}
-              >
-                建立新的產品
-              </button>
-            </div>
-            <table className="table mt-4">
-              <thead>
-                <tr>
-                  <th width="120">分類</th>
-                  <th>產品名稱</th>
-                  <th width="120">原價</th>
-                  <th width="120">售價</th>
-                  <th width="100">是否啟用</th>
-                  <th width="120">編輯</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.map((product) => (
-                  <tr key={product.id}>
-                    <td>{product.category}</td>
-                    <td>{product.title}</td>
-                    <td className="text-end">{product.origin_price}</td>
-                    <td className="text-end">{product.price}</td>
-                    <td>
-                      {product.is_enabled ? (
-                        <span className="text-success">啟用</span>
-                      ) : (
-                        <span>未啟用</span>
-                      )}
-                    </td>
-                    <td>
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary btn-sm"
-                          onClick={() => openModal(product, "edit")}
-                        >
-                          編輯
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-danger btn-sm"
-                          onClick={() => openModal(product, "delete")}
-                        >
-                          刪除
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <Pagination pagination={pagination} changePage={getProductData} />
-          </div>
-        </div>
+      {/* 登入表單頁面 */}
+      {!isAuth ? (
+        <Login getProducts={getProducts} setIsAuth={setIsAuth} />
       ) : (
-        <div className="container login mt-5">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="col-8">
-              <form id="form" className="form-signin" onSubmit={handleSubmit}>
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button
-                  className="btn btn-lg btn-primary w-100 mt-3"
-                  type="submit"
-                >
-                  登入
-                </button>
-              </form>
-            </div>
+        <div className="container">
+          <h2>產品列表</h2>
+          {/* 新增產品按鈕 */}
+          <div className="text-end mt-4">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => openModal('create', INITIAL_TEMPLATE_DATA)}
+            >
+              建立新的產品
+            </button>
           </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2025~∞ - 六角學院</p>
+          {/* 產品列表表格 */}
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">分類</th>
+                <th scope="col">產品名稱</th>
+                <th scope="col">原價</th>
+                <th scope="col">售價</th>
+                <th scope="col">是否啟用</th>
+                <th scope="col">編輯</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.category}</td>
+                  <th scope="row">{item.title}</th>
+                  <td>{item.origin_price}</td>
+                  <td>{item.price}</td>
+                  <td className={`${item.is_enabled && 'text-success'}`}>
+                    {item.is_enabled ? '啟用' : '未啟用'}
+                  </td>
+                  <td>
+                    <div
+                      className="btn-group"
+                      role="group"
+                      aria-label="Basic example"
+                    >
+                      <button
+                        type="button"
+                        className="btn btn-outline-primary btn-sm"
+                        onClick={() => openModal('edit', item)}
+                      >
+                        編輯
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                        onClick={() => openModal('delete', item)}
+                      >
+                        刪除
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination pagination={pagination} onChangePage={getProducts} />
         </div>
       )}
+      {/*!-- Modal互動視窗 --*/}
       <ProductModal
         modalType={modalType}
-        templateData={templateData}
-        onCloseModal={closeModal}
-        onInputChange={handleInputChange}
-        onFileChange={handleFileChange}
-        onImageChange={handleImageChange}
-        onAddImage={handleAddImage}
-        onRemoveImage={handleRemoveImage}
-        onUpdateProduct={updateProductData}
-        onDeleteProduct={delProductData}
+        templateProduct={templateProduct}
+        getProducts={getProducts}
+        closeModal={closeModal}
       />
     </>
   );
--- week4/vite.config.js
+++ week4/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/week4/',
   plugins: [react()],
-})
+  server: {
+    open: true, // 加這一行，run dev才會自動開網頁
+  },
+});
