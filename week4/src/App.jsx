d12143cd64bedf7b3d6d37a0fa01c85d889f8576
joashfan <hakuro100823@gmail.com>
2026-04-05 21:53:06 +0800

    week4

--- week4/src/App.jsx
+++ week4/src/App.jsx
@@ -4,8 +4,8 @@ import * as bootstrap from "bootstrap";
 import "./assets/style.css";
 
 // 1. 引入抽離出去的組件
-import ProductModal from "./components/ProductModal.jsx";
-import Pagination from "./components/Pagination.jsx";
+import ProductModal from "./component/ProductModal.jsx";
+import Pagination from "./component/Pagination.jsx";
 
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
 const API_PATH = "joashcat";
