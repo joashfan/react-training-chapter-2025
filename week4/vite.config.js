36ac660bead73bfeda21740ac9b5f886ceb58660
joashfan <hakuro100823@gmail.com>
2026-04-05 18:03:15 +0800

    week4

--- week4/vite.config.js
+++ week4/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    // 請把 '/react-training-chapter-2025/week4/' 換成你真實的 GitHub 儲存庫名稱
-  base: '/react-training-chapter-2025/week4/',
+    // 請把 './' 換成你真實的 GitHub 儲存庫名稱
+  base: './',
   plugins: [react()],
   server: {
     open: true, // 加這一行，run dev才會自動開網頁
